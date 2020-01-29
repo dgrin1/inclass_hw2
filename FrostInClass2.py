84c8aa4b227deb77f09cbd393833e44271c03d78
jdfrost18 <jdfrost@DESKTOP-Q8C18VE.localdomain>
2020-01-29 13:53:52 -0500

    Jon's harmonic sum

---
+++ FrostInClass2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+N=float(input("Insert end of sum:"))
+
+n=1
+answer=0
+while n<=N:
+    answer+=1/n
+    n+=1
+print(answer)
+
+
+# In[ ]:
+
+
+
+
