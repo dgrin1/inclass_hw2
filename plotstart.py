5e9ec7f3404854a759e3bd60e88df0606ecd40cd
dgrin1 <dgrin314@gmail.com>
2020-02-09 23:58:54 -0500

    itest.py

--- plotstart.py
+++ plotstart.py
@@ -17,61 +17,61 @@ y=np.abs(np.sin(x)*np.exp(-x))
 #y values list for bad sin theory
 ybad=np.abs(np.cos(x)*np.exp(-x))
 
-#two panel subplots
+##two panel subplots
 plt.subplot(211)
-plt.yscale('log')
+#plt.yscale('log')
 
 
 #coarsely gridded "data"
-xdata=np.logspace(1.e-5,2*3.14159,1000)
+xdata=np.logspace(1.e-5,2*3.14159,100)
 
 #make data points obey sin curve
 ydata=np.abs(np.sin(xdata))*np.exp(-xdata)
 
 #Add label
 a=plt.plot(x,y,label='theory')
-plt.setp(a,linewidth=8)
+plt.setp(a,linewidth=4)
 
 #plot "data"
 plt.plot(xdata,ydata,'b*',color='#FF0000',label='data',markersize=14)
 
 #Plot title
-plt.title('Climate change is a bummer')
+plt.title('Climate change is scary')
 
 #axis labels
-#plt.xlabel('Time')
+plt.xlabel('Time')
 plt.ylabel('Height of ocean in meters')
 
 #limits
 #plt.xlim([0,6])
-#plt.ylim([-1.3,1.3])
+#plt.ylim([.001,10])
 
 #put down a legend
 plt.legend(loc=1)
 
 #move to next panel
 plt.subplot(212)
-plt.yscale('log')
 #axis labels
-plt.xlabel(r'\omega t',fontsize=24)
+plt.xlabel(r'$\omega t$',fontsize=24)
 plt.ylabel('Height of ocean in meters')
 
 #limits again
-#plt.xlim([0,6])
-#plt.ylim([-1.3,1.3])
+plt.xlim([0,6])
+plt.ylim([.001,100])
 
 #plot bad cosine fit
-plt.plot(x,ybad,'r',label='bad theory')
-
+plt.plot(x,y,'r',label='bad theory')
+plt.yscale('log')
+plt.legend(loc=1)
 #plot additional data points
-#plt.plot(xdata,ydata,'r^',label='data',markersize=14)
-#plt.errorbar(xdata,ydata,xerr=.05,yerr=.7,fmt='r^',capsize=5,markersize=14,label='data')
+plt.plot(xdata,ydata,'r^',label='data',markersize=14)
+plt.errorbar(xdata,ydata,xerr=.05,yerr=.007,fmt='r^',capsize=5,markersize=14,label='data')
 
 plt.grid()
-
-
-
-
+# 
+# 
+# 
+# 
 plt.show()
-plt.savefig('test.pdf',format='pdf')
+# plt.savefig('test.pdf',format='pdf')
 
